e40e466da2547939bd507f5810066dd21c1aad4a
ChuakP <bigtank7890@gmail.com>
2024-05-01 18:07:30 +0530

    create a security group

--- terraform_github_actions/main.tf
+++ terraform_github_actions/main.tf
@@ -1,6 +1,29 @@
 provider "aws" {
-  region = "ap-south-1"
+    region     = "${var.region}"
+    access_key = "${var.access_key}"
+    secret_key = "${var.secret_key}"
 }
+
+resource "aws_security_group" "rds_dt" {
+  name        = "rds-dt"
+  description = "Security group for dittodb RDS MySQL instance"
+
+  ingress {
+    description = "MySQL"
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 
 resource "aws_db_instance" "dittodb" {
   allocated_storage    = 10
@@ -14,4 +37,9 @@ resource "aws_db_instance" "dittodb" {
   storage_type         = "gp2"
   db_name              = "dittodb"
   skip_final_snapshot  = true
+  vpc_security_group_ids = [aws_security_group.rds_dt.id]
+
+  tags = {
+    Name = "DittoDB"
+  }
 }
