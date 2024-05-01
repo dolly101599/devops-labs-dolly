26d5534af0f93ae343060fb4f97487673e3aa234
ChuakP <bigtank7890@gmail.com>
2024-05-01 18:14:23 +0530

    create vpc and attach subnets and print output

--- terraform_github_actions/main.tf
+++ terraform_github_actions/main.tf
@@ -4,7 +4,46 @@ provider "aws" {
     secret_key = "${var.secret_key}"
 }
 
-resource "aws_security_group" "rds_dt" {
+resource "aws_vpc" "ditto_vpc" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "ditto_vpc"
+  }
+}
+
+resource "aws_subnet" "ditto_subnet1" {
+  vpc_id            = aws_vpc.ditto_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "ap-south-1a"
+
+  tags = {
+    Name = "ditto_subnet1"
+  }
+}
+
+resource "aws_subnet" "ditto_subnet2" {
+  vpc_id            = aws_vpc.ditto_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "ap-south-1b"
+
+  tags = {
+    Name = "ditto_subnet2"
+  }
+}
+
+resource "aws_db_subnet_group" "ditto_db_subnet_group" {
+  name       = "ditto-db-subnet-group"
+  subnet_ids = [aws_subnet.ditto_subnet1.id, aws_subnet.ditto_subnet2.id]
+
+  tags = {
+    Name = "Ditto DB Subnet Group"
+  }
+}
+
+resource "aws_security_group" "rds_ditto" {
   name        = "rds-dt"
   description = "Security group for dittodb RDS MySQL instance"
 
@@ -24,7 +63,6 @@ resource "aws_security_group" "rds_dt" {
   }
 }
 
-
 resource "aws_db_instance" "dittodb" {
   allocated_storage    = 10
   engine               = "mysql"
@@ -37,7 +75,9 @@ resource "aws_db_instance" "dittodb" {
   storage_type         = "gp2"
   db_name              = "dittodb"
   skip_final_snapshot  = true
-  vpc_security_group_ids = [aws_security_group.rds_dt.id]
+  vpc_security_group_ids = [aws_security_group.rds_ditto.id]
+  db_subnet_group_name = aws_db_subnet_group.ditto_db_subnet_group.name
+  
 
   tags = {
     Name = "DittoDB"
