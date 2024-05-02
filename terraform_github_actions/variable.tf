c910496757b8e9b798e502cc304a042222a364f6
DiptoChakrabarty <diptochuck123@gmail.com>
2024-05-02 16:09:23 +0530

    add a variable tf

---
+++ terraform_github_actions/variable.tf
@@ -0,0 +1,9 @@
+variable "access_key" {
+    description = "Access key of AWS user"
+}
+variable "secret_key" {
+    description = "Secret key of AWS user"
+}
+variable "region" {
+    description = "AWS region"
+}
