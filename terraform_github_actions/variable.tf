5911cc3b4579be07cebdccb9c0ba8ead10ce0f19
DiptoChakrabarty <diptochuck123@gmail.com>
2024-05-04 10:49:15 +0530

    remove variables file

--- terraform_github_actions/variable.tf
+++
@@ -1,9 +0,0 @@
-variable "access_key" {
-    description = "Access key of AWS user"
-}
-variable "secret_key" {
-    description = "Secret key of AWS user"
-}
-variable "region" {
-    description = "AWS region"
-}
